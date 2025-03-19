d3930a8716fa69521d1ddb5cfcee905aee6ad1e3
silvak <silvak.jeg@gmail.com>
2025-03-19 12:46:03 -0400

    update network

--- src/networksList.js
+++ src/networksList.js
@@ -1,20 +1,26 @@
 export const NETWORKS = [
-    {
-        name: "Polygon zkEVM Cardona Testnet",
-        rpcUrl: "https://etherscan.cardona.zkevm-rpc.com",
-        chainId: 2442,
-        tokenName: "ETH",
-    },
-    {
-        name: "Ethereum Mainnet",
-        rpcUrl: "https://mainnet.infura.io/v3/YOUR_INFURA_KEY",
-        chainId: 1,
-        tokenName: "ETH",
-    },
-    {
-        name: "Ethereum Testnet Goerli",
-        rpcUrl: "https://goerli.drpc.org",
-        chainId: 5,
-        tokenName: "ETH",
-    },
+  {
+    name: "Polygon zkEVM Cardona Testnet",
+    rpcUrl: "https://etherscan.cardona.zkevm-rpc.com",
+    chainId: 2442,
+    tokenName: "ETH",
+  },
+  {
+    name: "Ethereum Mainnet",
+    rpcUrl: "https://mainnet.infura.io/v3/YOUR_INFURA_KEY",
+    chainId: 1,
+    tokenName: "ETH",
+  },
+  {
+    name: "Ethereum Testnet Goerli",
+    rpcUrl: "https://goerli.drpc.org",
+    chainId: 5,
+    tokenName: "ETH",
+  },
+  {
+    name: "Sepolia",
+    rpcUrl: "https://sepolia.infura.io",
+    chainId: 11155111,
+    tokenName: "sepoliaETH",
+  },
 ];
