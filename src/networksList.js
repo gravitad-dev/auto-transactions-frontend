5cc183e9575d409f5d8de0cf7cc606aefb95f6ef
silvak <silvak.jeg@gmail.com>
2025-03-26 14:27:37 -0400

    update network

--- src/networksList.js
+++ src/networksList.js
@@ -24,4 +24,18 @@ export const NETWORKS = [
     tokenName: "KNRT",
     tokenAddress: "0x54de10FADF4Ea2fbAD10Ebfc96979D0885dd36fA",
   },
+  {
+    name: "Base Mainnet USDC",
+    rpcUrl: "https://mainnet.base.org",
+    chainId: 8453,
+    tokenName: "USDC",
+    tokenAddress: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+  },
+  {
+    name: "Base Mainnet USDT",
+    rpcUrl: "https://mainnet.base.org",
+    chainId: 8453,
+    tokenName: "USDT",
+    tokenAddress: "0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2",
+  },
 ];
